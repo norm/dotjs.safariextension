16291ccce34cd2aa01e5f7e47edb99cb20f5f3ea
Mark Norman Francis <norm@cackhanded.net>
2011-03-27 22:55:35 +0100

    Allow for injected CSS as well as JS.

--- dotjs.js
+++ dotjs.js
@@ -1,5 +1,7 @@
+var dotjs = 'http://localhost:3131/'+window.location.host.replace('www.','');
+$('head').append("<link rel='stylesheet' href='" + dotjs + '.css' + "'>");
 $.ajax({
-  url: 'http://localhost:3131/'+window.location.host.replace('www.','')+'.js',
+  url: dotjs + '.js',
   dataType: 'text',
   success: function(d){
     $(function(){ eval(d) })
